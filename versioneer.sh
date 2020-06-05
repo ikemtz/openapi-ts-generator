35c21ae43fbfc9dfe7ab44337cfa6bd814229a97
IkeMtz <ikemtz@outlook.com>
2020-06-04 22:59:04 -0400

    Resolving last batch of cicd issues

---
+++ versioneer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+echo '** NPM Package Versioneer Script **'
+ls ./package.json
+
+echo BuildNumber $1
+buildNumber=$1
+value='s/\"version\"\: \"[0-9.]*\"/"version\"\: \"X\"/m'
+value="${value/X/$buildNumber}"\
+
+$(sed -i "$value" ./lib/package.json)
