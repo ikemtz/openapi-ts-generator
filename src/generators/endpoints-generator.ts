5eb8b953a08409b75f836dc6dae760fbdc6a7c96
IkeMtz <ikemtz@outlook.com>
2021-06-30 10:12:04 -0400

    Adding dedupe logic to endpoint generator

--- src/generators/endpoints-generator.ts
+++ src/generators/endpoints-generator.ts
@@ -4,12 +4,28 @@ import { BaseGenerator } from './base-generator';
 
 export class EndPointsGenerator extends BaseGenerator<{ paths: IPath[]; }> {
   public readonly GeneratorName = 'EndPointsGenerator';
-  private readonly tsRegex = /.ts$/;
+  public readonly endpointIdentifierRegex = /[A-z0-9_-]*$/;
   constructor(options: IGeneratorOptions) {
     super(options, options.templates?.endpoints);
   }
 
   public generate(templateData: ITemplateData): string | null {
-    return super.generateFile(`${this.generatorOptions.outputPath}/endpoints.ts`, { paths: templateData.paths });
+    const paths = this.eliminateDupes(templateData);
+    return super.generateFile(`${this.generatorOptions.outputPath}/endpoints.ts`, { paths: paths });
+  }
+
+  public eliminateDupes(templateData: ITemplateData): IPath[] {
+    const sortedTemplateData = [...templateData.paths.sort((x, y) =>
+      x.endpoint.toUpperCase() < y.endpoint.toUpperCase() ? -1 : 1)];
+    const result: IPath[] = [];
+    sortedTemplateData.forEach(val => {
+      if (result.findIndex(f => f.tag === val.tag) > -1) {
+        const endpointIdentifier = (this.endpointIdentifierRegex.exec(val.endpoint) || [])[0];
+        result.push({ ...val, tag: `${val.tag}_${endpointIdentifier || 'dupe'}` });
+      } else {
+        result.push(val);
+      }
+    });
+    return result;
   }
 }
