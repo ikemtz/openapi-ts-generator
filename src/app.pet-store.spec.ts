9af17ae7f0b484a1fd1a7a87668c6cf928874347
IkeMtz <ikemtz@outlook.com>
2022-03-29 08:11:36 -0400

    increasing code coverage

---
+++ src/app.pet-store.spec.ts
@@ -0,0 +1,27 @@
+import { mkdirSync } from 'fs';
+import { generateTsModels, nrsrxTypeFilterCallBack, nrsrxValuePropertyTypeFilterCallBack } from '.';
+import { ValidateFiles } from './app.spec';
+import { IGeneratorOptions } from './models/generator-options';
+
+const accountGenerationOptionsFactory = (): IGeneratorOptions => ({
+  openApiJsonUrl: 'https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/examples/v3.0/petstore.json',
+  outputPath: './jest_output/pet-store/',
+  typeFilterCallBack: nrsrxTypeFilterCallBack,
+  valuePropertyTypeFilterCallBack: nrsrxValuePropertyTypeFilterCallBack,
+  genAngularFormGroups: true,
+});
+
+describe('Url Based - Full Integration Tests', () => {
+  describe('Sample PetStore Service', () => {
+    it('should generate files', async () => {
+      const options = accountGenerationOptionsFactory();
+      try {
+        mkdirSync(options.outputPath, { recursive: true });
+      } catch {
+        // ignore
+      }
+      await generateTsModels(options);
+      ValidateFiles(options);
+    });
+  });
+});
