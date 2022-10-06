f0175cefe359599715474bab6adf05cc7c687e8d
IkeMtz <ikemtz@outlook.com>
2022-10-06 07:59:35 -0400

    Adding support for entities

---
+++ src/tests/app.mstr-acct.models.spec.ts
@@ -0,0 +1,27 @@
+import { mkdirSync } from 'fs';
+import { generateTsModels, nrsrxTypeFilterCallBack, nrsrxValuePropertyTypeFilterCallBack } from '..';
+import { ValidateFiles } from './app.spec';
+import { IGeneratorOptions } from '../models/generator-options';
+
+const accountGenerationOptionsFactory = (): IGeneratorOptions => ({
+  openApiJsonFileName: './open-api-spec-docs/mstr-acct.json',
+  outputPath: './jest_output/acct/',
+  typeFilterCallBack: nrsrxTypeFilterCallBack,
+  valuePropertyTypeFilterCallBack: nrsrxValuePropertyTypeFilterCallBack,
+  genAngularFormGroups: true,
+});
+
+describe('Url Based - Full Integration Tests', () => {
+  describe('MasterCorp Account Service - Models', () => {
+    it('should generate files', async () => {
+      const options = accountGenerationOptionsFactory();
+      try {
+        mkdirSync(options.outputPath, { recursive: true });
+      } catch {
+        // ignore
+      }
+      await generateTsModels(options);
+      ValidateFiles(options);
+    });
+  });
+});
