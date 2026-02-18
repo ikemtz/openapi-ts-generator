98e738b2ed620840f2dafd4953a5142037f38dd0
Isaac Martinez (@ikemtz) <ikemtz@outlook.com>
2026-02-17 20:58:09 -0500

    Fixing linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,26 @@
 import { defineConfig } from 'eslint/config';
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import { rules } from 'eslint-config-prettier';
 
 export default defineConfig([
   js.configs.recommended,
   tseslint.configs.recommended,
   tseslint.configs.recommendedTypeChecked,
   tseslint.configs.strictTypeChecked,
+  tseslint.configs.stylisticTypeChecked,
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       parser: tseslint.parser,
       parserOptions: {
+        projectService: true,
         project: './tsconfig.json',
+        tsconfigRootDir: './src',
       },
     },
     plugins: {
       '@typescript-eslint': tseslint.plugin,
     },
   },
-  { ignores: ['node_modules/**', 'dist/**', './coverage/**', 'lib/**', 'jest_output/**', 'jest.config.js', 'eslint.config.mjs'] },
+  { ignores: ['node_modules/**', 'dist/**', './coverage/**', 'lib/**', 'jest_output/**', 'jest.config.mjs', 'eslint.config.mjs'] },
 ]);
